3ff1a963c82d033419c51771bd24ad337947bd5e
ItzMari0 <msajr@outlook.com>
2023-02-07 18:31:36 -0300

    Requisito 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,19 @@
+db.produtos.updateMany(
+  {},
+  { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
+);
+
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $set: { "vendasPorDia.3": 60 }},
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["bovino"] } },
+  { $set: { "vendasPorDia.6": 120 }},
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, vendasPorDia: 1, _id: 0 },
+);
