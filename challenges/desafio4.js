33a97d1a8b19043f4ce333ec2d9c9bf539b6fc37
ItzMari0 <msajr@outlook.com>
2023-02-07 15:35:34 -0300

    lint novamente

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,4 +1,5 @@
 db.produtos.find(
-  { vendidos: { $gte: 50, $lte: 100 }},
-  { nome: 1, vendidos: 1, _id: 0 })
+  { vendidos: { $gte: 50, $lte: 100 } },
+  { nome: 1, vendidos: 1, _id: 0 }
+  )
     .sort({ vendidos: 1 });
